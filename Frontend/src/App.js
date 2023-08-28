b8755f565344a8561b6c75757439292741fa12ef
sinasezza <s.e.sezza121@gmail.com>
2023-08-28 22:52:22 +0330

    commit number 5: 	_1_some changes in app.css to beautify app. 	_2_added fetch method to app.js to fetch all Tasks. 	_3_installed django-cors-headers package in Backend and wrote main 	configurations for that in settings.py.

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,21 +2,54 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoList : [],
+      activeItem : {
+        id : null,
+        title : '',
+        description : '',
+        createdOn : '',
+        lastModified : '',
+        completed: false,
+      },
+      editing : false,
+    }
+    this.fetchTasks = this.fetchTasks.bind(this);
+  }
+
+  componentWillMount() {
+    this.fetchTasks();
+  }
+
+  fetchTasks() {
+    console.log('fetching ...');
+    fetch("http://127.0.0.1:8000/api/task-list/")
+    .then(response => response.json())
+    .then(data => console.log(`Data is ${data}`))
+    .catch(err => console.log(`Error : ${err}`))
+
+  }
+
   render() {
     return (
       <div className='container'>
         <div className='task-container'>
-          <div className='form-wrapper'>
-            <form  method="post" id='form'> 
-              <div className='flex-wrapper'>
-                <div style={{flex : 6}}>
-                  <input type="text" name="title" id="title" className='form-control' placeholder='task title'/>
+          <div id="form-wrapper">
+            <form id="form">
+              <div className="flex-wrapper">
+                <div style={{ flex: 6 }}>
+                  <input className="form-control" id="title" type="text" name="title" placeholder="Add task.." />
                 </div>
-                <div style={{flex : 1}}>
-                  <input id='submit' className='btn btn-warning' type="submit" name='add' value="add" />
+
+                <div style={{ flex: 1 }}>
+                  <input id="submit" className="btn btn-warning" type="submit" name="Add" />
                 </div>
               </div>
             </form>
+
           </div>
 
           <div className='list-wrapper'>
